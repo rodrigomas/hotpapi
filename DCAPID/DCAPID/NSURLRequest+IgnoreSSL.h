905a0c0cc728560788cec225ac724863574b77e2
Rodrigo Marques <rodrigomas@gmail.com>
2014-01-13 20:20:58 +0000

    Add Invalid SSL Support

---
+++ DCAPID/DCAPID/NSURLRequest+IgnoreSSL.h
@@ -0,0 +1,14 @@
+//
+//  NSURLRequest+IgnoreSSL.h
+//  DCAPID
+//
+//  Created by Rodrigo Marques on 1/12/14.
+//  Copyright (c) 2014 Ande Tecnologia. All rights reserved.
+//
+
+#ifndef DCAPID_NSURLRequest_IgnoreSSL_h
+#define DCAPID_NSURLRequest_IgnoreSSL_h
+
+
+
+#endif
---
+++ DCAPID/DCAPID/NSURLRequest+IgnoreSSL.m
@@ -0,0 +1,9 @@
+//
+//  NSURLRequest+IgnoreSSL.m
+//  DCAPID
+//
+//  Created by Rodrigo Marques on 1/12/14.
+//  Copyright (c) 2014 Ande Tecnologia. All rights reserved.
+//
+
+#include <stdio.h>
