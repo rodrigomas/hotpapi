5293a79fe95fc6f9a54a0c7a2a0ce14c9a654ab1
Rodrigo Marques <rodrigomas@gmail.com>
2014-02-04 20:56:38 -0200

    Crypt

---
+++ DCAPID/DCAPID/cryptlib.h
@@ -0,0 +1,14 @@
+//
+//  cryptlib.h
+//  DCAPID
+//
+//  Created by Rodrigo Marques on 2/3/14.
+//  Copyright (c) 2014 Ande Tecnologia. All rights reserved.
+//
+
+#ifndef DCAPID_cryptlib_h
+#define DCAPID_cryptlib_h
+
+
+
+#endif
